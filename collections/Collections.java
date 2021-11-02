1d2d2290ace9392794f28518c5e0e4c472c1f3eb
Kong M Vang <kongvang2@csus.edu>
2021-11-02 01:11:04 -0700

    separated Movie class from Collections class

--- collections/Collections.java
+++ collections/Collections.java
@@ -4,52 +4,6 @@ import java.io.*;
 import java.util.*;
 import java.io.IOException;
 
-class Movie{
-	private String title;
-	private String director;
-	private int year;
-	
-	public Movie(String title, String director, int year){
-		this.title=title;
-		this.director=director;
-		this.year=year;
-	}
-	
-	//default constructor
-	public Movie(){
-		this("null", "null", -1);
-	}
-	
-	/******* setters *******/
-	public void setTitle(String title){
-		this.title=title;
-	}
-	
-	public void setDirector(String director){
-		this.director=director;
-	}
-	
-	public void setYear(int year){
-		this.year=year;
-	}
-	
-	/****** getters *****/
-	public String getTitle(){
-		return this.title;
-	}
-	
-	public String getDirector(){
-		return this.director;
-	}
-	
-	public int getYear(){
-		return this.year;
-	}
-	
-	public String toString(){
-		return this.title + ", " + this.director + ", " + this.year;
-	}
-}
 
 public class Collections{
 	public static void main (String[] args){
